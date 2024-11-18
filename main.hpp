96152b78f5ff8031b5f80a5c99ccfec2075f4ad0
bramzil <brahim.amzil@gmail.com>
2024-11-18 22:26:47 +0100

    push header file

---
+++ main.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bramzil <bramzil@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 18:55:29 by bramzil           #+#    #+#             */
+/*   Updated: 2024/11/09 11:51:43 by bramzil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef MAIN_HPP
+# define MAIN_HPP
+
+# include <string>
+# include <vector>
+# include <stdio.h>
+# include <sstream>
+# include <iostream>
+# include <unistd.h>
+# include <arpa/inet.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include "Request.hpp"
+
+# endif
